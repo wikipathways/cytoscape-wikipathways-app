4e8ec14da4e0c4a97170269397d0c9fbcae77274
Samad Lotia <samad.lotia@gladstone.ucsf.edu>
2013-08-02 17:05:49 +0000

    Added loading of pathways

--- src/main/java/org/wikipathways/cytoscapeapp/internal/webclient/CyWPClient.java
+++ src/main/java/org/wikipathways/cytoscapeapp/internal/webclient/CyWPClient.java
@@ -7,13 +7,15 @@ import org.cytoscape.io.webservice.SearchWebServiceClient;
 import org.cytoscape.io.webservice.swing.AbstractWebServiceGUIClient;
 
 import java.util.List;
+import java.io.InputStream;
 
+import javax.swing.ListSelectionModel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.JCheckBox;
 import javax.swing.JComboBox;
 import javax.swing.JTable;
-import javax.swing.table.DefaultTableModel;
+import javax.swing.table.AbstractTableModel;
 import javax.swing.JButton;
 import javax.swing.JScrollPane;
 import java.awt.GridBagLayout;
@@ -21,12 +23,17 @@ import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseAdapter;
 
 import org.cytoscape.work.Task;
 import org.cytoscape.work.TaskMonitor;
 import org.cytoscape.work.TaskIterator;
 
+import org.cytoscape.io.read.CyNetworkReader;
+
 import org.wikipathways.cytoscapeapp.internal.CyActivator;
+import org.wikipathways.cytoscapeapp.internal.webclient.WPClient.PathwayRef;
 
 public class CyWPClient extends AbstractWebServiceGUIClient implements NetworkImportWebServiceClient, SearchWebServiceClient {
   static final String[] RESULTS_TABLE_COLUMN_NAMES = {"Pathway Name", "Species"};
@@ -34,7 +41,8 @@ public class CyWPClient extends AbstractWebServiceGUIClient implements NetworkIm
   final JCheckBox speciesCheckBox = new JCheckBox("Only: ");
   final JComboBox speciesComboBox = new JComboBox();
   final JButton searchButton = new JButton("Search");
-  final JTable resultsTable = new JTable(new DefaultTableModel(RESULTS_TABLE_COLUMN_NAMES, 0));
+  final PathwayRefsTableModel tableModel = new PathwayRefsTableModel();
+  final JTable resultsTable = new JTable(tableModel);
   WPClient client;
 
   public CyWPClient() {
@@ -64,6 +72,17 @@ public class CyWPClient extends AbstractWebServiceGUIClient implements NetworkIm
     };
     searchButton.addActionListener(performSearch);
     searchField.addActionListener(performSearch);
+
+    resultsTable.setSelectionMode(ListSelectionModel.SINGLE_SELECTION); 
+    resultsTable.addMouseListener(new MouseAdapter() {
+      public void mouseClicked(MouseEvent e) {
+        if (e.getClickCount() != 2)
+          return;
+        final TaskIterator taskIterator = new TaskIterator();
+        taskIterator.append(new LoadPathway(taskIterator));
+        CyActivator.taskMgr.execute(taskIterator);
+      }
+    });
 
     super.gui = new JPanel(new GridBagLayout());
     EasyGBC c = new EasyGBC();
@@ -114,12 +133,7 @@ public class CyWPClient extends AbstractWebServiceGUIClient implements NetworkIm
       searchButton.setEnabled(false);
       searchButton.setText("Searching...");
 
-      final DefaultTableModel dm = (DefaultTableModel) resultsTable.getModel();
-      for (int i = 0; i < dm.getRowCount(); i++) {
-        dm.removeRow(i);
-      }
-
-      List<WPClient.PathwayRef> pathwayRefs = null;
+      List<PathwayRef> pathwayRefs = null;
       if (speciesCheckBox.isSelected()) {
         final String species = (String) speciesComboBox.getSelectedItem();
         pathwayRefs = client.freeTextSearch(query, species);
@@ -127,12 +141,74 @@ public class CyWPClient extends AbstractWebServiceGUIClient implements NetworkIm
         pathwayRefs = client.freeTextSearch(query);
       }
 
-      for (final WPClient.PathwayRef pathwayRef : pathwayRefs) {
-        dm.addRow(new Object[] { pathwayRef.getName(), pathwayRef.getSpecies() });
-      }
+      tableModel.setPathwayRefs(pathwayRefs);
 
       searchButton.setEnabled(true);
       searchButton.setText("Search");
+    }
+  }
+
+  class LoadPathway extends InterruptableTask {
+    final TaskIterator taskIterator;
+    public LoadPathway(TaskIterator taskIterator) {
+      this.taskIterator = taskIterator;
+    }
+
+    public void interruptableRun(TaskMonitor monitor) throws Exception {
+      final PathwayRef pathwayRef = tableModel.getSelectedPathwayRef();
+      if (pathwayRef == null)
+        return;
+      monitor.setTitle("Opening from WikiPathways: " + pathwayRef.getName());
+      final InputStream gpmlStream = client.loadPathway(pathwayRef);
+      final CyNetworkReader netReader = CyActivator.netReaderMgr.getReader(gpmlStream, pathwayRef.getName() + ".gpml");
+      if (netReader == null)
+        throw new Exception("Could not load GPML file");
+      taskIterator.append(netReader);
+    }
+  }
+
+  class PathwayRefsTableModel extends AbstractTableModel {
+    List<PathwayRef> pathwayRefs = null;
+
+    public void setPathwayRefs(List<PathwayRef> pathwayRefs) {
+      this.pathwayRefs = pathwayRefs;
+      super.fireTableDataChanged();
+    }
+
+    public int getRowCount() {
+      return (pathwayRefs == null) ? 0 : pathwayRefs.size();
+    }
+
+    public int getColumnCount() {
+      return 2;
+    }
+
+    public Object getValueAt(int row, int col) {
+      final PathwayRef pathwayRef = pathwayRefs.get(row);
+      switch(col) {
+        case 0: return pathwayRef.getName();
+        case 1: return pathwayRef.getSpecies();
+        default: return null;
+      }
+    }
+
+    public String getColumnName(int col) {
+      switch(col) {
+        case 0: return "Pathway Name";
+        case 1: return "Species";
+        default: return null;
+      }
+    }
+
+    public boolean isCellEditable(int row, int col) {
+      return false;
+    }
+
+    public PathwayRef getSelectedPathwayRef() {
+      final int row = resultsTable.getSelectedRow();
+      if (row < 0)
+        return null;
+      return pathwayRefs.get(row);
     }
   }
 }
