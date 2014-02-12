fd2fd7b8a34824946c20f6706f80f5a3dc14564c
Samad Lotia <samad.lotia@gladstone.ucsf.edu>
2014-02-11 17:18:03 -0800

    Ported WPClientREST to new API

---
+++ src/main/java/org/wikipathways/cytoscapeapp/impl/WPClientRESTImpl.java
@@ -0,0 +1,157 @@
+package org.wikipathways.cytoscapeapp.impl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.io.IOException;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.ParserConfigurationException;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.HttpMethod;
+import org.apache.commons.httpclient.methods.GetMethod;
+import org.apache.commons.httpclient.NameValuePair;
+
+import org.cytoscape.work.TaskMonitor;
+
+import org.wikipathways.cytoscapeapp.WPClient;
+import org.wikipathways.cytoscapeapp.WPPathway;
+import org.wikipathways.cytoscapeapp.ResultTask;
+
+public class WPClientRESTImpl implements WPClient {
+  protected static final String BASE_URL = "http://www.wikipathways.org/wpi/webservice/webservice.php/";
+
+  final DocumentBuilder xmlParser;
+  final HttpClient client;
+
+  protected static DocumentBuilder newXmlParser() throws ParserConfigurationException {
+    final DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+    factory.setValidating(false);
+    final DocumentBuilder builder = factory.newDocumentBuilder();
+    return builder;
+  }
+  
+  public WPClientRESTImpl() {
+    try {
+      xmlParser = newXmlParser();
+    } catch (ParserConfigurationException e) {
+      throw new IllegalArgumentException("Failed to build XML parser", e);
+    }
+    client = new HttpClient();
+  }
+
+  /**
+   * A convenience class for issuing cancellable REST calls.
+   */
+  protected abstract class ReqTask<T> extends ResultTask<T> {
+    protected NameValuePair[] makeNameValuePairs(final String[] args) {
+      final NameValuePair[] nvPairs = new NameValuePair[args.length / 2];
+      for (int i = 0; i < args.length; i += 2) {
+        nvPairs[i / 2] = new NameValuePair(args[i], args[i + 1]);
+      }
+      return nvPairs;
+    }
+
+    protected HttpMethod req = null;
+
+    protected Document xmlGet(final String url, final String ... args) throws IOException, SAXException {
+      // build our get request
+      req = new GetMethod(url);
+      req.setQueryString(makeNameValuePairs(args));
+
+      try {
+        client.executeMethod(req);
+        return xmlParser.parse(req.getResponseBodyAsStream());
+      } finally {
+        req.releaseConnection();
+        req = null;
+      }  
+    }
+
+    public void cancel() {
+      final HttpMethod req2 = req; // copy the ref to req so that it doesn't become null when trying to abort it
+      if (req2 != null) {
+        req2.abort();
+      }
+    }
+  }
+
+  public ResultTask<List<String>> newSpeciesTask() {
+    return new ReqTask<List<String>>() {
+      protected List<String> checkedRun(final TaskMonitor monitor) throws Exception {
+        final Document doc = xmlGet(BASE_URL + "listOrganisms");
+        final Node responseNode = doc.getFirstChild();
+        final NodeList organismNodes = responseNode.getChildNodes(); 
+        final List<String> result = new ArrayList<String>();
+        for (int i = 0; i < organismNodes.getLength(); i++) {
+          final Node organismNode = organismNodes.item(i);
+          result.add(organismNode.getTextContent());
+        }
+        return result;
+      }
+    };
+  }
+
+  public ResultTask<List<WPPathway>> newFreeTextSearchTask(final String query, final String species) {
+    return new ReqTask<List<WPPathway>>() {
+      protected List<WPPathway> checkedRun(final TaskMonitor monitor) throws Exception {
+        final Document doc = xmlGet(BASE_URL + "findPathwaysByText", "query", query, "species", species == null ? "" : species);
+        final Node responseNode = doc.getFirstChild();
+        final NodeList resultNodes = responseNode.getChildNodes(); 
+        final List<WPPathway> result = new ArrayList<WPPathway>();
+        for (int i = 0; i < resultNodes.getLength(); i++) {
+          String id = "", revision = "", name = "", species = "";
+          final Node resultNode = resultNodes.item(i);
+          final NodeList argNodes = resultNode.getChildNodes();
+          for (int j = 0; j < argNodes.getLength(); j++) {
+            final Node argNode = argNodes.item(j);
+            final String argName = argNode.getNodeName();
+            final String argVal = argNode.getTextContent();
+            if (argName.equals("ns2:id")) {
+              id = argVal;
+            } else if (argName.equals("ns2:revision")) {
+              revision = argVal;
+            } else if (argName.equals("ns2:name")) {
+              name = argVal;
+            } else if (argName.equals("ns2:species")) {
+              species = argVal;
+            }
+          }
+          result.add(new WPPathway(id, revision, name, species));
+        }
+        return result;
+      }
+    };
+  }
+
+  protected static Node findChildNode(final Node parentNode, final String nodeName) {
+    final NodeList nodes = parentNode.getChildNodes();
+    for (int i = 0; i < nodes.getLength(); i++) {
+      final Node node = nodes.item(i);
+      if (node.getNodeName().equals(nodeName))
+        return node;
+    }
+    return null;
+  }
+
+  public ResultTask<InputStream> newLoadPathwayTask(final WPPathway pathway) {
+    return new ReqTask<InputStream>() {
+      protected InputStream checkedRun(final TaskMonitor monitor) throws Exception {
+        final Document doc = xmlGet(BASE_URL + "getPathway", "pwId", pathway.getId(), "revision", pathway.getRevision());
+        final Node responseNode = doc.getFirstChild();
+        final Node pathwayNode = responseNode.getFirstChild(); 
+        final Node gpmlNode = findChildNode(pathwayNode, "ns2:gpml");
+        final String gpmlContents = gpmlNode.getTextContent();
+        return new ByteArrayInputStream(gpmlContents.getBytes());
+      }
+    };
+  }
+}
