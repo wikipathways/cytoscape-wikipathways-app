5505b8de8e8dffc38867e8bb04a71d57783a7568
Samad Lotia <samad.lotia@gladstone.ucsf.edu>
2014-02-22 16:45:47 -0800

    Revamped GpmlToPathway

---
+++ src/main/java/org/wikipathways/cytoscapeapp/internal/io/DelayedVizProp.java
@@ -0,0 +1,42 @@
+package org.wikipathways.cytoscapeapp.internal.io;
+
+import org.cytoscape.model.CyNode;
+import org.cytoscape.model.CyEdge;
+import org.cytoscape.model.CyIdentifiable;
+
+import org.cytoscape.view.model.CyNetworkView;
+import org.cytoscape.view.model.View;
+import org.cytoscape.view.model.VisualProperty;
+
+class DelayedVizProp {
+  final CyIdentifiable netObj;
+  final VisualProperty<?> prop;
+  final Object value;
+  final boolean isLocked;
+
+  public DelayedVizProp(final CyIdentifiable netObj, final VisualProperty<?> prop, final Object value, final boolean isLocked) {
+    this.netObj = netObj;
+    this.prop = prop;
+    this.value = value;
+    this.isLocked = isLocked;
+  }
+
+  public static void applyAll(final CyNetworkView netView, final Iterable<DelayedVizProp> delayedProps) {
+    for (final DelayedVizProp delayedProp : delayedProps) {
+      View<?> view = null;
+      if (delayedProp.netObj instanceof CyNode) {
+        final CyNode node = (CyNode) delayedProp.netObj;
+        view = netView.getNodeView(node);
+      } else if (delayedProp.netObj instanceof CyEdge) {
+        final CyEdge edge = (CyEdge) delayedProp.netObj;
+        view = netView.getEdgeView(edge);
+      }
+
+      if (delayedProp.isLocked) {
+        view.setLockedValue(delayedProp.prop, delayedProp.value);
+      } else {
+        view.setVisualProperty(delayedProp.prop, delayedProp.value);
+      }
+    }
+  }
+}
