5da640aa5e51d69e4b091da790d7630135793aeb
Samad Lotia <samad.lotia@gladstone.ucsf.edu>
2014-05-02 14:42:25 -0700

    Added true support for toggling

--- src/main/java/org/wikipathways/cytoscapeapp/internal/ToggleShapesTask.java
+++ src/main/java/org/wikipathways/cytoscapeapp/internal/ToggleShapesTask.java
@@ -18,15 +18,44 @@ public class ToggleShapesTask extends AbstractTask {
   public void run(TaskMonitor monitor) {
     final CyNetwork net = netView.getModel();
     final CyTable nodeTbl = net.getDefaultNodeTable();
+    final boolean areGPMLShapesShown = areGPMLShapesShown();
     for (final View<CyNode> nodeView : netView.getNodeViews()) {
       final CyNode node = nodeView.getModel();
       final Boolean isShape = nodeTbl.getRow(node.getSUID()).get("IsGPMLShape", Boolean.class);
       if (!Boolean.TRUE.equals(isShape))
         continue;
-      nodeView.setLockedValue(BasicVisualLexicon.NODE_VISIBLE, Boolean.FALSE);
+      nodeView.setLockedValue(BasicVisualLexicon.NODE_VISIBLE, !areGPMLShapesShown);
     }
     netView.updateView();
   }
 
   public void cancel() {}
+
+  /**
+   * Counts the number of shown GPML shapes vs hidden; if the number of shown is more,
+   * return true.
+   *
+   * This is done because there is no reliable way to know if the toggle shapes task
+   * was performed on a given network. Setting a network attribute may not work,
+   * because the network view can be destroyed and all of the visual properties of its
+   * nodes without affecting the state of the network attribute.
+   */
+  boolean areGPMLShapesShown() {
+    final CyNetwork net = netView.getModel();
+    final CyTable nodeTbl = net.getDefaultNodeTable();
+    int shownCount = 0;
+    int hiddenCount = 0;
+    for (final View<CyNode> nodeView : netView.getNodeViews()) {
+      final CyNode node = nodeView.getModel();
+      final Boolean isShape = nodeTbl.getRow(node.getSUID()).get("IsGPMLShape", Boolean.class);
+      if (!Boolean.TRUE.equals(isShape))
+        continue;
+      if (Boolean.FALSE.equals(nodeView.getVisualProperty(BasicVisualLexicon.NODE_VISIBLE))) {
+        hiddenCount++;
+      } else {
+        shownCount++;
+      }
+    }
+    return shownCount > hiddenCount;
+  }
 }
