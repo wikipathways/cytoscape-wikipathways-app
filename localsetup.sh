67af3919bbc9febbe9c42d6c1ce2efbf16055fc4
De <denise.slenter@maastrichtuniversity.nl>
2022-04-06 14:01:40 +0200

    updated jdom version and maven-bundle-plugin

--- localsetup.sh
+++ localsetup.sh
@@ -1,12 +1,10 @@
 # pathvisio core
-mvn install:install-file -Dfile=localRepo/org.pathvisio.core.jar -DgroupId=org.pathvisio -DartifactId=pathvisio-core -Dversion=3.2.2 -Dpackaging=jar
+mvn install:install-file -Dfile=localRepo/org.pathvisio.core.jar -DgroupId=org.pathvisio -DartifactId=pathvisio-core -Dversion=3.3.0 -Dpackaging=jar
 
 # bridgedb 
-mvn install:install-file -Dfile=localRepo/org.bridgedb.jar -DgroupId=org.bridgedb -DartifactId=bridgedb -Dversion=2.0.0 -Dpackaging=jar
-mvn install:install-file -Dfile=localRepo/org.bridgedb.bio.jar -DgroupId=org.bridgedb -DartifactId=bridgedb-bio -Dversion=2.0.0 -Dpackaging=jar
+mvn install:install-file -Dfile=localRepo/org.bridgedb.jar -DgroupId=org.bridgedb -DartifactId=bridgedb -Dversion=3.0.13 -Dpackaging=jar
+mvn install:install-file -Dfile=localRepo/org.bridgedb.bio.jar -DgroupId=org.bridgedb -DartifactId=bridgedb-bio -Dversion=3.0.13 -Dpackaging=jar
 mvn install:install-file -Dfile=localRepo/derby.jar -DgroupId=derby -DartifactId=derby -Dversion=10.4 -Dpackaging=jar
 
 # xml parsing
-mvn install:install-file -Dfile=localRepo/com.springsource.org.jdom-1.1.0.jar -DgroupId=com.springsource -DartifactId=org.jdom -Dversion=1.1.0 -Dpackaging=jar
-
-
+mvn install:install-file -Dfile=localRepo/jdom-2.0.6.1.jar -DgroupId=org.jdom -DartifactId=jdom2 -Dversion=2.0.6.1 -Dpackaging=jar
